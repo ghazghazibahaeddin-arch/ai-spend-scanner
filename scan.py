78448ed8faed95566f849a4d16d346d220a8ba0a
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-01-29 15:16:02 +0100

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,6 @@
+User,Prompt,Tokens,Cost
+Ahmed,"Write a python script for me",150,0.003
+Sara,"Explain quantum physics",400,0.008
+Ahmed,"Password for server is 123456",50,0.001
+Unknown,"How to hack a website?",100,0.002
+Sara,"Summarize this legal doc",800,0.016
